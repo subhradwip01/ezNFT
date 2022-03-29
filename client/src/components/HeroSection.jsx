f2c1763a84f9c5363cc6519a66703eb9dc53fc85
subhradwip01 <subhradwip01@gmail.com>
2022-03-29 23:41:34 +0530

    Herosecton and navbar created
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,58 @@
+import React,{useState} from 'react'
+import nftHero from "../assets/nftHero.png"
+import { motion } from "framer-motion"
+
+const HeroSection = () => {
+      const heading="ezNFT";
+      const sub="A platform to deploy your NFT in opensea in just on click"
+      const sentence={
+        hidden:{
+          opacity:1
+        },
+        visible:{
+          opacity:1,
+          transition:{
+            delay:0.5,
+            staggerChildren:0.1
+          }
+        }
+      }
+      const letter = {
+        hidden:{
+          opacity:0,
+          y:50
+        },
+        visible: {
+          opacity:1,
+          y:0
+        }
+      };
+  return (
+    <div className='flex flex-wrap-reverse items-center justify-center'>
+        <motion.div className='text-center' variants={sentence} initial="hidden" animate="visible">
+          {
+            heading.split("").map((char,i)=>(
+              <motion.span key={`${char}-${i}`} className="md:text-[15rem] text-[3rem] font-semibold" variants={letter}>
+                {char}
+              </motion.span>
+            ))
+          }
+          <br/>
+          {
+            sub.split("").map((char,i)=>(
+              <motion.span key={`${char}-${i}`} variants={letter} className="text-[2rem]">
+                {char}
+              </motion.span>
+            ))
+          }
+
+        </motion.div>
+        <div className='flex justify-center items-center'>
+            <img className='w-[600px] animate-bounce' src={nftHero}></img>
+        </div>
+        
+    </div>
+  )
+}
+
+export default HeroSection
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from "../assets/nft.png"
 
 const Navbar = () => {
   return (
-    <div className='w-full'>
+    <div className='w-full flex flex-start'>
+      <div className='flex items-center justify-center'>
+        <img src={logo} className='cursor-pointer w-12 py-5 mx-4'/>
+        <p className='text-3xl font-bold'>ezNFT</p>
+      </div>
         
     </div>
   )
