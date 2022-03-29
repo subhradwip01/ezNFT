d8a4e7c52a7226846e3f4d36f2c1cac749374a2e
subhradwip01 <subhradwip01@gmail.com>
2022-03-29 10:40:44 +0530

    Logo added and removed nodemodules from root dir
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './component/Navbar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar/>
+    </>
   )
 }
 
--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full '>
-
+    <div className='w-full'>
+        
     </div>
   )
 }
