50120423a536c0a665a4889f7e7194eea6bc7c84
subhradwip01 <subhradwip01@gmail.com>
2022-04-04 21:16:45 +0530

    Router added
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import Navbar from './component/Navbar'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { Routes,Route } from 'react-router-dom'
+import Home from './components/Home'
+import Form from "./components/Form"
+
 
 const App = () => {
   return (
-    <>
+    <div className='app-background'>
       <Navbar/>
-    </>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/mint' element={<Form/>}/>
+          {/* <Route path='/' element={<Home/>}/> */}
+        </Routes>
+      <Footer/>
+    </div>
   )
 }
 
