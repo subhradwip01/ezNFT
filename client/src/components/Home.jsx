74283970f3bdc9355be4adc173299f322ed5ba63
subhradwip01 <subhradwip01@gmail.com>
2022-04-04 21:17:45 +0530

    Hoem page created
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import Services from './Services'
+
+const Home = () => {
+  return (
+    <>
+        <HeroSection/>
+        <Services/>
+    </>
+  )
+}
+
+export default Home
