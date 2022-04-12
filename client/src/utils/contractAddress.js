2d8a275560c2b0ec7f0e61527f9dabe0428c195e
subhradwip01 <subhradwip01@gmail.com>
2022-04-12 20:43:52 +0530

    New address and abi added
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- client/src/utils/contractAddress.js
+++
@@ -1,1 +0,0 @@
-export const contractAddress = 0x85168Cd07f1B9Bf43c591340FC667F3f3eEeFaeA
---
+++ client/src/utils/contractDetails.js
@@ -0,0 +1,5 @@
+import NFTPunk from "./NFTPunk.json"
+
+
+export const abi = NFTPunk.abi
+export const contractAddress = 0xF6F2a7f5AFefa0fE6597D85d8606E0F250c66D1c
