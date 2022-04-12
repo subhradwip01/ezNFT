d018275b2daef22d7168581f91bdb96f91830e4f
subhradwip01 <subhradwip01@gmail.com>
2022-04-12 23:33:42 +0530

    Loader component added
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="animate-spin w-[35px] h-[35px] border-4 rounded-[50%] border-[#DC5D76] border-t-[white]"/>
+      <div className="animate-spin w-[35px] h-[35px] border-4 rounded-[50%] border-[black] border-t-[white]"/>
     </div>
   )
 }
