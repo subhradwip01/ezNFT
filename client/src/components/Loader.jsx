702775975bfb96a93e5e493966c39bb040bf92f0
subhradwip01 <subhradwip01@gmail.com>
2022-04-12 23:26:10 +0530

    Loader added
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,13 @@
+
+  
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="animate-spin w-[35px] h-[35px] border-4 rounded-[50%] border-[#DC5D76] border-t-[white]"/>
+    </div>
+  )
+}
+
+export default Loader
