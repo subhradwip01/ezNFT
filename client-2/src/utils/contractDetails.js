274d404070370656e38d2f0c2e89203b41e414c7
subhradwip01 <subhradwip01@gmail.com>
2022-04-16 23:50:26 +0530

    Bug fixed
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- client-2/src/utils/contractDetails.js
+++ client-2/src/utils/contractDetails.js
@@ -2,4 +2,4 @@ import NFTPunk from "./NFTPunk.json"
 
 
 export const abi = NFTPunk.abi
-export const contractAddress = 0xF6F2a7f5AFefa0fE6597D85d8606E0F250c66D1c
+export const contractAddress = "0xF6F2a7f5AFefa0fE6597D85d8606E0F250c66D1c"
