e6732b67af09f62c366cbcc2528e258e66af4362
subhradwip01 <subhradwip01@gmail.com>
2022-04-28 23:46:08 +0530

    Protected routes added
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- client-2/src/App.js
+++ client-2/src/App.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { Routes,Route } from 'react-router-dom'
 import Home from './components/Home'
 import Form from "./components/Form"
-
+import AllMints from "./components/AllMints"
+import { NFTContext } from './context/NFTContext'
 
 const App = () => {
+  const ntx=useContext(NFTContext)
   return (
     <div className='app-background'>
       <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          {ntx.connectedAccount && <>
           <Route path='/mint' element={<Form/>}/>
-          {/* <Route path='/' element={<Home/>}/> */}
+          <Route path='/allMint' element={<AllMints/>}/>
+          </>}
         </Routes>
+        
       <Footer/>
     </div>
   )
